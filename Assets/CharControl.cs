802fa9497e161fe943020e14f83b366199301bc4
Vladyslav <serveladik@gmail.com>
2020-09-16 16:09:49 +0300

    Movement

---
+++ Assets/CharControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharControl : MonoBehaviour
+{
+    public Joystick joystick;
+    private Rigidbody rb;
+    public float sensitivityToSpeed;
+
+    void Start()
+    {
+        joystick=FindObjectOfType<Joystick>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerControl();
+    }
+    void PlayerControl()
+    {
+        rb.velocity = new Vector3 (joystick.Horizontal * sensitivityToSpeed/3,rb.velocity.y,joystick.Vertical * sensitivityToSpeed/3);
+    }
+}
