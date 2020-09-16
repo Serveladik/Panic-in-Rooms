1f154981e36a782de2421b5409a08b796c8a56d4
Vladyslav <serveladik@gmail.com>
2020-09-16 16:37:54 +0300

    rotation

--- Assets/CharControl.cs
+++ Assets/CharControl.cs
@@ -7,11 +7,16 @@ public class CharControl : MonoBehaviour
     public Joystick joystick;
     private Rigidbody rb;
     public float sensitivityToSpeed;
+    private Quaternion playerRotation;
 
     void Start()
     {
-        joystick=FindObjectOfType<Joystick>();
+        if(joystick==null)
+        {
+            joystick=FindObjectOfType<Joystick>();
+        }
         rb = GetComponent<Rigidbody>();
+        playerRotation = transform.rotation;
     }
 
     // Update is called once per frame
@@ -22,5 +27,20 @@ public class CharControl : MonoBehaviour
     void PlayerControl()
     {
         rb.velocity = new Vector3 (joystick.Horizontal * sensitivityToSpeed/3,rb.velocity.y,joystick.Vertical * sensitivityToSpeed/3);
+        //Look at movement vector
+        if(Input.touchCount>=1)
+        {
+            playerRotation = Quaternion.LookRotation(rb.velocity);
+            Debug.Log("ROT");
+        }
+        else
+        {
+            transform.rotation = playerRotation;
+        }
+        
+    }
+    void PlayerRotation()
+    {
+
     }
 }
